b0f455693a755932007df91fc5d8aff11880b548
marisa_b <marisakbrantley@gmail.com>
2021-06-25 18:22:43 -0700

    Initial commit of main.js

---
+++ main.js
@@ -0,0 +1,125 @@
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+
+  set isCheckedOut(value) {
+    this._isCheckedOut = value;
+ }
+
+  get ratings() {
+    return this._ratings;
+  }
+ 
+  toggleCheckOutStatus() {
+    this.isCheckedOut = !this.isCheckedOut;
+  }
+
+ getAverageRating() {
+   let ratingsSum = this.ratings.reduce((accumulator, rating) => accumulator + rating);
+   return ratingsSum / this.ratings.length;
+ }
+
+  addRating(value) {
+    this.ratings.push(value);
+  }
+}
+
+
+class Book extends Media {
+  constructor(author, title, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+
+}
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+
+
+class Movie extends Media {
+  constructor(director, title, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+    }
+
+  get director() {
+    return this._director;
+  }
+
+  get runTime() {
+    return this._runTime;
+  }
+}
+
+const speed = new Movie ('Jan de Bont','Speed', 116);
+speed.toggleCheckOutStatus();
+console.log(speed._isCheckedOut);
+
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+
+speed.getAverageRating();
+console.log(speed.getAverageRating());
+
+
+class CD extends Media {
+  constructor(artist, title, songs) {
+    super(title);
+    this._artist = artist;
+    this._songs =[];
+  }
+
+  get artist() {
+    return this._artist;
+  }
+
+  get songs() {
+    return this._songs;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
